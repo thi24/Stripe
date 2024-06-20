b77ee803db5d4fd2cec32298922fb16e3dfc16a6
RamonPH01 <rap4517@thi.de>
2024-06-20 09:06:25 +0200

    auslagern paymentIntent logik -Ramon

--- src/main/java/com/benevolo/resource/StripeResource.java
+++ src/main/java/com/benevolo/resource/StripeResource.java
@@ -1,15 +1,13 @@
-package com.benevolo;
+package com.benevolo.resource;
 
+import com.benevolo.util.StripeLogic;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.stripe.Stripe;
 import com.stripe.exception.StripeException;
-import com.stripe.model.PaymentIntent;
 import com.stripe.model.Refund;
-import com.stripe.param.PaymentIntentCreateParams;
 import com.stripe.param.RefundCreateParams;
-import jakarta.inject.Inject;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
@@ -18,7 +16,6 @@ import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 
-import java.io.IOException;
 
 
 @Produces(MediaType.APPLICATION_JSON)
@@ -34,28 +31,10 @@ public class StripeResource {
     @Path("/payment-intent")
     @Consumes(MediaType.APPLICATION_JSON)
     public Response createPaymentIntent(String payload) {
-        try {
+            StripeLogic logic = new StripeLogic(payload);
+            return logic.createPaymentIntent();
 
-            // Deserialisierung des JSON-Payloads
-            ObjectMapper mapper = new ObjectMapper();
-            JsonNode requestData = mapper.readTree(payload);
 
-            // Extrahieren der erforderlichen Informationen aus dem Payload
-            int amount = requestData.get("amount").asInt();
-            String currency = requestData.get("currency").asText();
-            String productDescription = requestData.get("product").asText();
-            Stripe.apiKey = STRIPE_SECRET;
-
-            PaymentIntentCreateParams params = PaymentIntentCreateParams.builder().setAmount((long) amount).setCurrency(currency).setDescription(productDescription).build();
-
-            PaymentIntent paymentIntent = PaymentIntent.create(params);
-            String clientSecret = paymentIntent.getClientSecret();
-            String id = paymentIntent.getId();
-            String jsonResponse = "{\"client_secret\": \"" + clientSecret + "\", \"id\": \"" + id + "\"}";
-            return Response.ok().entity(jsonResponse).build();
-        } catch (IOException | StripeException e) {
-            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity("Fehler beim Erstellen des PaymentIntent: " + e.getMessage()).build();
-        }
     }
     @POST
     @Path("/refund")
